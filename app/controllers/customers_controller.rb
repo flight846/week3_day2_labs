985b096463f5c3fa97a6a0a43aa66db587e5280f
Yazid Ismail <flight846@gmail.com>
2016-06-21 09:29:32 +0000

    Add form

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -5,6 +5,14 @@ class CustomersController < ApplicationController
     end
     
     def create
+        customer_params = params.require(:customer).permit(:name, :email, :address)
+        @customer = Customer.new(customer_params)
+
+        if @customer.save
+            redirect_to @customer, notice: 'customer was successfully created.'
+        else
+            render :new
+        end
     end
     
     # READ
@@ -17,13 +25,27 @@ class CustomersController < ApplicationController
     end
     
     def edit
+        @customer = Customer.find(params[:id])
     end
     
     # UPDATE
     def update
+        customer_params = params.require(:customer).permit(:name, :email, :address)
+        # @customer = Customer.new(customer_params)
+        @customer = Customer.find(params[:id])
+
+        if @customer.update(customer_params)
+            redirect_to @customer, notice: 'customer was successfully update.'
+        else
+            render :edt
+        end
     end
     
     # DESTROY
     def destroy
+        customer = Customer.find(params[:id])
+        customer.destroy
+        
+        redirect_to customers_path, notice: "customer deleted"
     end
 end
